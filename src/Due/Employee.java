51e523135dbcedfe1bf86abf2af115aef67ba480
Konrad Herba <konrad.herba@amartus.com>
2015-07-08 10:31:08 +0200

    Empl import removed

--- src/Due/Employee.java
+++ src/Due/Employee.java
@@ -1,16 +1,14 @@
 package Due;
 
-import java.io.*;
-
 public class Employee implements java.io.Serializable
 {
-   public String name;
-   public String address;
-   public transient int SSN;
-   public int number;
-   public void mailCheck()
-   {
-      System.out.println("Mailing a check to " + name
+	public String name;
+	public String address;
+	public transient int SSN;
+	public int number;
+	public void mailCheck()
+	{
+		System.out.println("Mailing a check to " + name
                            + " " + address);
-   }
+	}
 }
